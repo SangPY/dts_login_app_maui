afcd2868415291549b7f8dbb044c0d1fa195d365
sangdothanh95 <sangdothanh95@gmail.com>
2025-12-23 10:41:03 +0700

    crrate project

---
+++ dts_login_app.API/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using dts_login_app.API.Data;
+using dts_login_app.API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dts_login_app.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+        public UserController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get() => Ok(await appDbContext.Users.ToListAsync());
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Add(User user)
+        {
+            if (user != null)
+            {
+                var result = appDbContext.Users.Add(user).Entity;
+                await appDbContext.SaveChangesAsync();
+                return Ok(result);
+            }
+            return BadRequest("Invalid Request");
+        }
+
+        [HttpGet("{email}/{password}")]
+        public async Task<ActionResult<User>> Login(string email, string password)
+        {
+            if (email is not null && password is not null)
+            {
+                var user = await appDbContext.Users
+                    .Where(x => x.Email!.ToLower().Equals(email.ToLower()) && x.Password == password)
+                    .FirstOrDefaultAsync();
+                return user != null ? Ok(user) : NotFound("User not found");
+            }
+            return BadRequest("Invalid Request");
+        }
+    }
+}
---
+++ dts_login_app.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using dts_login_app.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace dts_login_app.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+    }
+}
