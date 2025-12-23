01ea985dbb31d63287239d2a613c93af07256a0c
sangdothanh95 <sangdothanh95@gmail.com>
2025-12-23 09:54:13 +0700

    Add project files.

---
+++ dts_login_app.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace dts_login_app.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
